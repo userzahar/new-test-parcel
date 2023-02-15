caed900ec80f395e4669f91483534439061afe94
userzahar <110730909+userzahar@users.noreply.github.com>
2023-02-15 14:20:02 +0200

    jr

--- src/index.js
+++ src/index.js
@@ -1,63 +1,28 @@
-import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
-const gallery = document.querySelector('.gallery');
-const button = document.querySelector('.button');
-button.addEventListener('click', search);
-// const options = {
-//   totalItems: 10,
-//   itemsPerPage: 10,
-//   visiblePages: 20,
-//   page: 1,
-//   centerAlign: false,
-//   firstItemClassName: 'tui-first-child',
-//   lastItemClassName: 'tui-last-child',
-//   template: {
-//     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-//     currentPage:
-//       '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-//     moveButton:
-//       '<a href="#" class="tui-page-btn tui-{{type}}">' +
-//       '<span class="tui-ico-{{type}}">{{type}}</span>' +
-//       '</a>',
-//     disabledMoveButton:
-//       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-//       '<span class="tui-ico-{{type}}">{{type}}</span>' +
-//       '</span>',
-//     moreButton:
-//       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-//       '<span class="tui-ico-ellip">...</span>' +
-//       '</a>',
-//   },
-// };
+const li = document.querySelector('.pag__child');
 
-async function search(page) {
-  const pokemon = await fetch(
+async function getData(page = 1) {
+  const response = await fetch(
     `https://pixabay.com/api/?key=33289628-97fffc14136600725dd3f07c9&q=dog&page=${page}`
   );
-  const jsonPokemon = await pokemon.json();
-  //   console.log('🚀 ~ jsonPokemon', jsonPokemon);
+  const data = await response.json();
 
-  const hits = jsonPokemon.hits;
-  //   console.log('🚀 ~ hits', hits);
-  renderCard(hits, jsonPokemon);
+  return data;
 }
 
-function renderCard(arrObj, json) {
-  const card = arrObj
-    .map(({ largeImageURL }) => {
-      return `<img src="${largeImageURL}"></img>`;
-    })
-    .join('');
-  const options = {
-    totalItems: json.totalHits,
-    itemsPerPage: json.hits.length,
-    visiblePages: 5,
-  };
-  gallery.innerHTML = card;
-  const pagination = new Pagination('pagination', options);
-  pagination.on('afterMove', evt => {
-    const { page } = evt;
-    // pagination.movePageTo(page);
-    search(page);
-  });
+async function pagination(dataObj) {
+  const data = await dataObj;
+  const { totalHits, hits } = await data;
+  const totalPages = (await totalHits) / (await hits.length);
+
+  function renderLi(totalPges) {
+    console.log('🚀 ~ totalPages', totalPages);
+
+    for (let i = 1; i <= totalPages; i += 1) {
+      li.insertAdjacentHTML('afterend', `<li>${i}</li>`);
+    }
+  }
+  renderLi(totalPages);
+  // function activeButton() {}
+  // function changePage() {}
 }
+pagination(getData());
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,32 +1,63 @@
-import Pagination from 'tui-pagination';
-console.log('🚀 ~ Pagination', Pagination);
-import 'tui-pagination/dist/tui-pagination.css';
+// import Pagination from 'tui-pagination';
+// import 'tui-pagination/dist/tui-pagination.css';
+// const gallery = document.querySelector('.gallery');
+// const button = document.querySelector('.button');
+// button.addEventListener('click', search);
+// // const options = {
+// //   totalItems: 10,
+// //   itemsPerPage: 10,
+// //   visiblePages: 20,
+// //   page: 1,
+// //   centerAlign: false,
+// //   firstItemClassName: 'tui-first-child',
+// //   lastItemClassName: 'tui-last-child',
+// //   template: {
+// //     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+// //     currentPage:
+// //       '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+// //     moveButton:
+// //       '<a href="#" class="tui-page-btn tui-{{type}}">' +
+// //       '<span class="tui-ico-{{type}}">{{type}}</span>' +
+// //       '</a>',
+// //     disabledMoveButton:
+// //       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+// //       '<span class="tui-ico-{{type}}">{{type}}</span>' +
+// //       '</span>',
+// //     moreButton:
+// //       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+// //       '<span class="tui-ico-ellip">...</span>' +
+// //       '</a>',
+// //   },
+// // };
 
-const options = {
-  totalItems: 10,
-  itemsPerPage: 10,
-  visiblePages: 10,
-  page: 1,
-  centerAlign: false,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      '</a>',
-  },
-};
+// async function search(page) {
+//   const pokemon = await fetch(
+//     `https://pixabay.com/api/?key=33289628-97fffc14136600725dd3f07c9&q=dog&page=${page}`
+//   );
+//   const jsonPokemon = await pokemon.json();
+//   //   console.log('🚀 ~ jsonPokemon', jsonPokemon);
 
-const pagination = new Pagination('pagination', options);
+//   const hits = jsonPokemon.hits;
+//   //   console.log('🚀 ~ hits', hits);
+//   renderCard(hits, jsonPokemon);
+// }
+
+// function renderCard(arrObj, json) {
+//   const card = arrObj
+//     .map(({ largeImageURL }) => {
+//       return `<img src="${largeImageURL}"></img>`;
+//     })
+//     .join('');
+//   const options = {
+//     totalItems: json.totalHits,
+//     itemsPerPage: json.hits.length,
+//     visiblePages: 5,
+//   };
+//   gallery.innerHTML = card;
+//   const pagination = new Pagination('pagination', options);
+//   pagination.on('afterMove', evt => {
+//     const { page } = evt;
+//     // pagination.movePageTo(page);
+//     search(page);
+//   });
+// }
